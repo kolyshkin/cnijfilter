eb8f109a5b3151e982561434ef85e0f65b1cbdcd
Kir Kolyshkin <kolyshkin@gmail.com>
2024-09-11 14:57:57 -0700

    cngpijmon/cnijnpr/cnijnpr/cnijnpr.c: fix -Wimplicit-int
    cnijnpr.c: In function ‘get_printer_devid’:
    cnijnpr.c:587:12: error: type of ‘fdusb’ defaults to ‘int’ [-Wimplicit-int]
      587 | static int get_printer_devid(fdusb)
          |            ^~~~~~~~~~~~~~~~~
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- cngpijmon/cnijnpr/cnijnpr/cnijnpr.c
+++ cngpijmon/cnijnpr/cnijnpr/cnijnpr.c
@@ -585,7 +585,7 @@ err01:
 /************************************************/
 /*             get_printer_devid               */
 /************************************************/
-static int get_printer_devid(fdusb)
+static int get_printer_devid(int fdusb)
 {
 	char	devid[1024];
 	char	*ivec = NULL;
